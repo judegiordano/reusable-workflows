b5c8a77aff3cbab68846ba762148c82d0595df2b
judegiordano <judegiordano@gmail.com>
2025-06-25 19:04:32 -0400

    bun init

---
+++ vectorize-codebase/src/config.ts
@@ -0,0 +1,10 @@
+import path from 'path'
+import fs from 'fs'
+
+export const WORKSPACE = process.env.GITHUB_WORKSPACE ?? ''
+export const SHA = process.env.GITHUB_SHA as string
+export const EXCLUDES = process.argv[2]?.split(',') ?? []
+// prepend workspace to only include this project's directory
+export const INCLUDES = (process.argv[3]?.split(',') ?? ['**/*']).map((entry) => path.join(WORKSPACE, entry))
+// write path
+export const DATA_PATH = fs.mkdirSync(path.join(WORKSPACE, '.vector_data'), { recursive: true })
